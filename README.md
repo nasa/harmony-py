22e01828340ade6d15eb24349b031b22d4b4188c
Ryan Waters <rmwaters@contractor.usgs.gov>
2021-01-26 09:00:18 -0600

    HARMONY-717: Added mocking library and notebook. WIP

---
+++ README.md
@@ -0,0 +1,3 @@
+# harmony-py
+
+`harmony_py.ipynb` is a notebook for demonstrating, documenting, and adding questions for how to approach a Harmony python library.
---
+++ harmony_py_mock.py
@@ -0,0 +1,43 @@
+import progressbar
+import time
+from getpass import getpass
+from halo import Halo
+
+
+class HarmonyRequest():
+
+    def __init__(self, params=None):
+        self.params = params
+
+
+    def authenticate(self, username=None, method=None):
+        if username is not None:
+            password = getpass('Password: ')
+            self._creds = {'username': username, 'password': password}
+            print('\'username\' and \'password\' accepted.')
+
+
+    @property
+    def params(self):
+        return self._params
+
+
+    @params.setter
+    def params(self, value):
+        self._params = value
+
+
+    def submit(self):
+        # spinner = Halo(text='Processing request... ', spinner='dots')
+        # spinner.start()
+        # time.sleep(5)
+        # spinner.stop()
+        print('Processing request:')
+        for i in progressbar.progressbar(range(100)):
+            time.sleep(0.08)
+        print('Request processing complete.')
+
+
+    @property
+    def output(self):
+        return self._output
