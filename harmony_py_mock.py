83aebc5bacf54d07ee53fffad70aa13d84e1fd95
Ryan Waters <rmwaters@contractor.usgs.gov>
2021-01-27 14:54:02 -0600

    HARMONY-717: Example notebook changes and additions. WIP

--- harmony_py_mock.py
+++ harmony_py_mock.py
@@ -4,11 +4,40 @@ from getpass import getpass
 from halo import Halo
 
 
+class HarmonyFormat():
+    zarr = None
+
+
+class Dataset():
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    def info(self):
+        pass
+
+    def summary(self):
+        pass
+
+    def visualize(self):
+        pass
+
+
+class Spatial():
+    def __init__(self, spatial):
+        self.spatial = spatial
+    
+    def visualize(self):
+        pass
+
+
 class HarmonyRequest():
+
+    format = HarmonyFormat
 
     def __init__(self, params=None):
         self.params = params
-
+        self.dataset = Dataset(None)
+        self.spatial = Spatial(None)
 
     def authenticate(self, username=None, password=None, netrc=None):
         if username is not None and password is None:
