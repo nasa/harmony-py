4aaccf18bdcf98afef65adc9ea4fafde60bb98e4
Ryan Waters <rmwaters@contractor.usgs.gov>
2021-03-18 21:20:08 -0500

    HARMONY-710: Modified the notebook helper library for new progress reporting and downloading.

--- examples/helper.py
+++ examples/helper.py
@@ -14,7 +14,7 @@ from rasterio.plot import show
 import requests
 
 
-def download_and_show_results(harmony_client, job_id):
+def download_and_show_results_prev(harmony_client, job_id):
     print('Waiting for the job to finish')
 
     status = harmony_client.status(job_id)
@@ -35,4 +35,19 @@ def download_and_show_results(harmony_client, job_id):
         with open(result['title'], 'wb') as f:
             print(f"  {result['title']}")
             f.write(session.get(result['href']).result().content)
+            show(rasterio.open(result['title']))
+
+
+def download_and_show_results(harmony_client, job_id):
+    print('Waiting for the job to finish')
+    results = harmony_client.result_json(job_id, show_progress=True)
+
+    print('Downloading results')
+    files = harmony_client.download_all(job_id)
+    for f in files:
+        f.result()
+
+    for result in [link for link in results['links'] if link['type'] == 'image/tiff']:
+        with open(result['title'], 'wb') as f:
+            print(f"  {result['title']}")
             show(rasterio.open(result['title']))
