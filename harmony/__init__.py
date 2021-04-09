f41c887cd10ef309b01a4b4c03e77b2a0471f393
Patrick Quinn <patrick@element84.com>
2021-04-09 11:55:48 -0400

    Version bump to v0.1.1

--- harmony/__init__.py
+++ harmony/__init__.py
@@ -1,5 +1,5 @@
 # Automatically updated by `make build`
-__version__ = "v0.1.0"
+__version__ = "v0.1.1"
 
 from harmony.config import Environment
 from harmony.harmony import BBox, Client, Collection, LinkType, Request
