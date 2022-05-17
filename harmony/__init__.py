aa3d332d4846a5b5ae256a3076468755674eea31
Chris Durbin <christopher.d.durbin@nasa.gov>
2022-05-17 16:39:49 -0400

    HARMONY-1124: Add support for setting arbitrary dimension subset parameters.

--- harmony/__init__.py
+++ harmony/__init__.py
@@ -2,5 +2,5 @@
 __version__ = "0.4.1"
 
 from harmony.config import Environment
-from harmony.harmony import BBox, Client, Collection, LinkType, Request
+from harmony.harmony import BBox, Client, Collection, LinkType, Request, Dimension
 from harmony.util import s3_components
