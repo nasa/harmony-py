868ef02fe5d1abe9ee246bb34b52cd751c3baba7
James Norton <james.norton@nasa.gov>
2024-05-01 14:56:15 -0400

    HARMONY-1756: Revert version that got overwritten by build

--- harmony/__init__.py
+++ harmony/__init__.py
@@ -1,5 +1,5 @@
 # Automatically updated by `make build`
-__version__ = "1756beta"
+__version__ = "0.4.13"
 
 from harmony.config import Environment
 from harmony.harmony import BBox, Client, Collection, LinkType, Dimension
