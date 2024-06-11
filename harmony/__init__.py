dfbf53a5f044b22886a4b26805e1d0b52428874f
Yonggang Liu <ygliuvt@gmail.com>
2024-06-11 10:58:10 -0400

    HARMONY-1745: As a HarmonyPy user I want to be able to access the Harmony OGC EDR API so that I can make WKT requests.

--- harmony/__init__.py
+++ harmony/__init__.py
@@ -2,6 +2,6 @@
 __version__ = "0.4.14"
 
 from harmony.config import Environment
-from harmony.harmony import BBox, Client, Collection, LinkType, Dimension
+from harmony.harmony import BBox, WKT, Client, Collection, LinkType, Dimension
 from harmony.harmony import Request, CapabilitiesRequest
 from harmony.util import s3_components
