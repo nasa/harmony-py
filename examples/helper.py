06fdd59aad05801c6e10c6425fef1d572f1a9e36
Yonggang Liu <ygliuvt@gmail.com>
2024-10-11 15:31:12 -0400

    HARMONY-1909: Updated all notebooks with the new helper import function.

--- examples/helper.py
+++ examples/helper.py
@@ -1,7 +1,18 @@
 import sys
 import os
+sys.path.append('..')
+
+import datetime as dt
+from getpass import getpass
+from glob import glob
+from time import sleep
+
+import ipyplot
+from ipywidgets import IntSlider, Password, Text
+from IPython.display import display, JSON
 import rasterio
 from rasterio.plot import show
+import requests
 
 def install_project_and_dependencies(project_root, libs=None):
     """
