a19fcd0b614a35d3b8bd8d1dd50bbf06bef7da99
vinny <vinny.inverso@gmail.com>
2024-11-21 08:17:55 -0500

    HARMONY-1929: Lint cleanup

--- harmony/harmony.py
+++ harmony/harmony.py
@@ -1317,13 +1317,14 @@ class Client:
             parse_result = parse.urlparse(url)
             is_opendap = parse_result.netloc.startswith('opendap')
             method = 'post' if is_opendap else 'get'
-            if is_opendap: # remove the query params from the URL and convert to dict
+            if is_opendap:  # remove the query params from the URL and convert to dict
                 new_url = parse.urlunparse(parse_result._replace(query=""))
                 data_dict = dict(parse.parse_qsl(parse.urlsplit(url).query))
             headers = {
                 "Accept-Encoding": "identity"
             }
-            with getattr(session, method)(new_url, data=data_dict, stream=True, headers=headers) as r:
+            with getattr(session, method)(
+                    new_url, data=data_dict, stream=True, headers=headers) as r:
                 with open(filename, 'wb') as f:
                     shutil.copyfileobj(r.raw, f, length=chunksize)
             if verbose and verbose.upper() == 'TRUE':
