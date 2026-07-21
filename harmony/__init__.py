7e0ed5bd6eecb9dce187a4ba7f04a509d6918c84
Matt Savoie <github@flamingbear.com>
2026-07-21 08:38:08 -0600

    Improvement: Switch to git tags for version during packaging. (#142)

--- harmony/__init__.py
+++ harmony/__init__.py
@@ -1,5 +1,9 @@
-# Automatically updated by `make build`
-__version__ = "1.4.0"
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("harmony-py")
+except PackageNotFoundError:  # running from a source tree that was never installed
+    __version__ = "0.0.0+unknown"
 
 from harmony.config import Environment
 from harmony.request import BBox, WKT, Collection, LinkType, Dimension, Request, \
